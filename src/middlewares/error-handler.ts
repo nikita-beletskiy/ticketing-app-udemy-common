596060056e51dd4160f5bc40525f0dc44ddc8d4e
Nikita Beletskiy <bel.nikita3197@gmail.com>
2020-12-29 16:30:00 +0300

    v1.0.0 commit
    This commit contains already relocated from auth service code (middlewares and errors folders).

---
+++ src/middlewares/error-handler.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from 'express';
+import { CustomError } from '../errors/custom-error';
+
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (err instanceof CustomError)
+    res.status(err.statusCode).send({ errors: err.arrangeErrors() });
+
+  res.status(400).send({ errors: [{ message: 'Somethig went wrong' }] });
+};
